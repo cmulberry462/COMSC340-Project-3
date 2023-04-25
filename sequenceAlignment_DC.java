05e9815d33be7fc8ae0c679495b198fcfe2242e0
cmulberry462 <90421969+cmulberry462@users.noreply.github.com>
2023-04-25 16:10:45 +0000

    comparison and timing code added

--- sequenceAlignment_DC.java
+++ sequenceAlignment_DC.java
@@ -7,14 +7,20 @@ import java.lang.Math;
 
 public class sequenceAlignment_DC {
 
-    private static char[] x = {'T', 'C', 'G', 'A', 'A', 'G', 'G', 'G', 'T', 'A'};
-    private static char[] y = {'C', 'T', 'G', 'T', 'G', 'T', 'A', 'C', 'A', 'A'};
+    private static char[] x = {'A', 'A'};
+    private static char[] y = {'T', 'A'};
     private static int m = x.length;
     private static int n = y.length;
+    static int comparisons;
 
     public static void main(String[] args) {
+        long start = System.nanoTime();
         int optimalCost = findOpt(0, 0);
+        long finish = System.nanoTime();
+        long elapsed = finish-start;
         System.out.println("The cost of an optimal alignment is: " + optimalCost);
+        System.out.println("Elapsed time: " + elapsed + " nanoseconds");
+        System.out.println(comparisons + " comparisons performed");
     }
 
 
@@ -22,19 +28,26 @@ public class sequenceAlignment_DC {
        int opt;
        int penalty;
         if (i==m) {
+            comparisons++;
             opt = 2*(n-j);
         } else if (j == n) {
+            comparisons++;
             opt = 2*(m-i);
         } else {
+            comparisons++;
             if (x[i] == y[j]) {
+                comparisons++;
                 penalty = 0;
             }else {
+                comparisons++;
                 penalty = 1;
             }
             int opt1 = findOpt(i+1, j+1) + penalty;
             int opt2 = findOpt(i+1, j) + 2;
             int opt3 = findOpt(i, j+1) + 2;
             opt = Math.min(Math.min(opt1, opt2), opt3);
+            comparisons++;
+            comparisons++;
         }
         return opt;
     }
